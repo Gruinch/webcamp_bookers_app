83509e53b5f15f13c512d2f795bdfe9eeafe82a0
Gruinch <sel.chemi86@gmail.com>
2021-03-15 06:47:11 +0000

    [Add]投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
   def new
-    @book=Book.new
   end
 
-  
   def index
+    @book=Book.all
+    @book=Book.new
   end
 
   def show
@@ -14,13 +14,14 @@ class BooksController < ApplicationController
     book=Book.new(book_params)
     book.save
     redirect_to book_path
-  end 
+  end
 
   def edit
   end
-  
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
-    
+  end
+
 end
